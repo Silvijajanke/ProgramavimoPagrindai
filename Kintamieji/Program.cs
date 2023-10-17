c1ad79f4450548ff77481484dccff811ff067f99
Jankai <Jankai@DESKTOP-61FU6DP>
2023-10-17 20:31:21 +0200

    Add project files.

---
+++ Kintamieji/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Kintamieji
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string vardas = "Silvija";
+            string pavarde = "Janke";
+            int amzius = 34;
+            float ugis = 164f;
+            float svoris = 54f;
+            string aukstoji_mokykla = "Kauno technologijos universitetas";
+            string akademines_grupes_kodas = "TBM8/1";
+            char kursas = '1';
+            string studiju_programos_pavadinimas = "Chemijos inzinerija";
+            int atsiskaitymu_kreditu_skaicius = 200;
+        }
+    }
+}
