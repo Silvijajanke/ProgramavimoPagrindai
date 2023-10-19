c5026bd256ad9289d244b5a6909fe593286a8fe2
Jankai <Jankai@DESKTOP-61FU6DP>
2023-10-19 11:05:39 +0200

    2 uzduotis

--- Isvedimas/Program.cs
+++ Isvedimas/Program.cs
@@ -7,21 +7,15 @@ namespace Isvedimas
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = System.Text.Encoding.Unicode;
-            // 1
-            Console.WriteLine("Labas, Silvija");
-
-            //2
-            int amzius = 34;
-            Console.WriteLine("Įvestas amžius {0}", amzius);
-
-            //3
-            int skaicius = 25;
-            Console.WriteLine("Skaičius {0}{0}{0}{0}{0}", skaicius);
-
-            //4
-            int a = 25;
-            Console.WriteLine($"Skaičius {a} {a} {a} {a} {a}");
+            string a = "Mantas";
+            Console.WriteLine("_________________");
+            Console.WriteLine("|                |");
+            Console.WriteLine("|                |");
+            Console.WriteLine("|  Mano vardas   |");
+            Console.WriteLine("|   (a)          |");
+            Console.WriteLine("|                |");
+            Console.WriteLine("|                |");
+            Console.WriteLine("_________________");
         }
     }
 }
