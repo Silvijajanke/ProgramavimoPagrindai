f3fc8dbf78f9b89d5a8ec3b4ed39e9df0aac9910
Jankai <Jankai@DESKTOP-61FU6DP>
2023-10-22 12:23:21 +0200

    07uzduotis

--- Isvedimas/Program.cs
+++ Isvedimas/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Isvedimas
 {
@@ -11,6 +8,33 @@ namespace Isvedimas
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.Unicode;
+            //uzduotis 1 
+            Console.WriteLine("Įveskite savo vardą:");
+            string vardas = Console.ReadLine();
+            Console.WriteLine("Įveskite savo amžių:");
+            int amzius = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Jūsų vardas {vardas}, o amžius {amzius}");
+
+
+            //uzduotis 2
+
+            Console.WriteLine("Įveskite bet kokį simbolį:");
+            char simbolis = Convert.ToChar(Console.ReadLine());
+            char o = ' ';
+            Console.WriteLine($"{simbolis} {simbolis} {simbolis}");
+            Console.WriteLine($"{simbolis} {o} {simbolis} ");
+            Console.WriteLine($"{simbolis} {simbolis} {simbolis}");
+
+            //uzduotis 3 
+
+            Console.WriteLine("Įveskite bet kokį skaičių:");
+            int skaicius = Convert.ToInt32(Console.ReadLine());
+            char o = ' ';
+            Console.WriteLine($"{skaicius} {skaicius} {skaicius} {skaicius}");
+            Console.WriteLine($"{skaicius} {o} {skaicius} ");
+            Console.WriteLine($"{skaicius} {o} {skaicius} ");
+            Console.WriteLine($"{skaicius} {o} {skaicius} ");
+            Console.WriteLine($"{skaicius} {skaicius} {skaicius} {skaicius}");
         }
     }
 }
