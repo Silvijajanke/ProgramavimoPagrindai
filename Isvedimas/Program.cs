c176e62f39807b419526352e3d72b11a04c4b112
Jankai <Jankai@DESKTOP-61FU6DP>
2023-10-22 11:08:50 +0200

    5 uzduotis

--- Isvedimas/Program.cs
+++ Isvedimas/Program.cs
@@ -10,8 +10,27 @@ namespace Isvedimas
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = System.Text.Encoding.Unicode;
-            Console.WriteLine("labas ir aČIŪ");
+            //1 uzduotis
+            int a = 108;
+            int b = 88;
+            int c = a + b;
+            int d = a - b;
+            int e = a * b;
+            double f = (double)a / b;
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            //2 uzduotis
+            int h = 8;
+            int j = (h * h);
+            Console.WriteLine(Math.Pow(j, 3));
+
+            //3 uzduotis
+            int u = 108;
+            int t = 88;
+            int r = 10;
+            Console.WriteLine(u * t * r);
         }
     }
 }
