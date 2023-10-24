de85aeedc0bdc29ced93bc4aa6ac778d59905a3a
Jankai <Jankai@DESKTOP-61FU6DP>
2023-10-24 10:31:49 +0200

    taisyta

--- Isvedimas/Program.cs
+++ Isvedimas/Program.cs
@@ -8,7 +8,12 @@ namespace Isvedimas
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.Unicode;
-            
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            string marke = "Fiat";
+            string modelis = "Fiat500";
+            int metai = 2008;
+            int rida = 151000;
+            Console.WriteLine($"Automobilio {marke} {modelis} ({metai} m.) rida - {rida}");
         }
     }
 }
